1cf7b85c3ff5d8a99888d239702d21f1b0f9faf1
julia-walti <91602293+julia-walti@users.noreply.github.com>
2021-11-28 13:13:46 +0100

    datasets loaders

--- scripts/helpers.py
+++ scripts/helpers.py
@@ -1,38 +1,78 @@
 import torch
 import torchvision
-from dataset import ImgDataset
-from torch.utils.data import Dataloader
 
-def value_to_class(v, foreground_threshold): #################################
+
+############################ PREPROCESSING ###########################################
+def value_to_class(v, foreground_threshold): 
+    """ Get a one-hot vector for the classes """
     df = np.sum(v)
     if df > foreground_threshold:
         return 1
     else:
         return 0
     
+def img_crop(im, w, h):
+    list_patches = []
+    imgwidth = im.shape[0]
+    imgheight = im.shape[1]
+    is_2d = len(im.shape) < 3
+    for i in range(0,imgheight,h):
+        for j in range(0,imgwidth,w):
+            if is_2d:
+                im_patch = im[j:j+w, i:i+h]
+            else:
+                im_patch = im[j:j+w, i:i+h, :]
+            list_patches.append(im_patch)
+    return list_patches 
+
+def transform(imgs, gt_imgs):
+    # Compute features for each image patch
+    # Extract patches from input images
+    patch_size = 16 # each patch is 16*16 pixels
+
+    img_patches = [img_crop(imgs[i], patch_size, patch_size) for i in range(n)]
+    gt_patches = [img_crop(gt_imgs[i], patch_size, patch_size) for i in range(n)]
+
+    # Linearize list of patches
+    img_patches = np.asarray([img_patches[i][j] for i in range(len(img_patches)) for j in range(len(img_patches[i]))])
+    gt_patches =  np.asarray([gt_patches[i][j] for i in range(len(gt_patches)) for j in range(len(gt_patches[i]))])
+
+    foreground_threshold = 0.25 # percentage of pixels > 1 required to assign a foreground label to a patch
+
+    X = np.asarray([extract_features_2d(img_patches[i]) for i in range(len(img_patches))])
+    Y = np.asarray([value_to_class(np.mean(gt_patches[i]), foreground_threshold) for i in range(len(gt_patches))])
+
+    # Print feature statistics
+    print('Computed ' + str(X.shape[0]) + ' features')
+    print('Feature dimension = ' + str(X.shape[1]))
+    print('Number of classes = ' + str(np.max(Y)))  #TODO: fix, length(unique(Y)) 
+
+    Y0 = [i for i, j in enumerate(Y) if j == 0]
+    Y1 = [i for i, j in enumerate(Y) if j == 1]
+    print('Class 0: ' + str(len(Y0)) + ' samples')
+    print('Class 1: ' + str(len(Y1)) + ' samples')
+
+    # Display a patch that belongs to the foreground class i.e with ones 
+    plt.imshow(gt_patches[Y1[2]], cmap='Greys_r')
+
+    # Display a patch that belongs to the background class i.e mostly zeros
+    #plt.imshow(gt_patches[Y0[2]], cmap='Greys_r')
+    
+    # Balancing and extract features?
+    return img_patches, gt_patches
+
+############################# CHECKPOINTS ###########################################
 def save_checkpoint(state, filename="my_checkpoint.pth.tar"):
     print("=> Saving checkpoint")
     torch.save(state, filename)
 
 def load_checkpoint(checkpoint, model):
     print("=> Loading checkpoint")
-    model.load_state_dict(checkpoint["state_dict"])    
-    
-def get_loaders(train_dir, train_gtdir, val_dir, val_gtdir, 
-    batch_size, train_transform, val_transform, num_workers=4, pin_memory=True):
-    train_ds = ImgDataset(
-        image_dir=train_dir, gt_dir=train_gtdir, transform=train_transform)
-    train_loader = DataLoader( 
-        train_ds, batch_size=batch_size, num_workers=num_workers,
-        pin_memory=pin_memory, shuffle=True)
-    val_ds = ImgDataset(
-        image_dir=val_dir, gt_dir=val_gtdir, transform=val_transform)
-    val_loader = DataLoader(
-        val_ds, batch_size=batch_size, num_workers=num_workers,
-        pin_memory=pin_memory, shuffle=False)
-    
-    return train_loader, val_loader
+    model.load_state_dict(checkpoint["state_dict"]) 
 
+
+
+############################ MODEL PERFORMANCE EVALUATION ###################################
 def check_accuracy(loader, model, device="cuda"):
     num_correct = 0
     num_pixels = 0
@@ -46,12 +86,14 @@ def check_accuracy(loader, model, device="cuda"):
             num_correct += (preds == y).sum
             num_pixels += torch.numel(preds)
             
-     print(
-         f"Got {num_correct}/{num_pixels} with acc {num_correct/num_pixels*100}:.2f")
+    print(f"Got {num_correct}/{num_pixels} with acc {num_correct/num_pixels*100}:.2f")
     model.train()
+
+
+
     
-def save_predictions_as_imgs():
-    
+def save_predictions_as_imgs(s):
+    s=3
     
 
 
--- scripts/run.py
+++ scripts/run.py
@@ -1,20 +1,41 @@
 import numpy as np
 import os
 from PIL import Image
-from torch.utils.data import Dataset
-from dataset import ImgDataset 
+from torch.utils import data
+from dataset import ImgDataset
+from dataset import TestDataset
 from helpers import *
 
+LEARNING_RATE = 1e-4
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+NUM_CHANNELS = 3  # RGB images
+PIXEL_DEPTH = 255
+NUM_LABELS = 2
+TRAINING_SIZE = 20
+VALIDATION_SIZE = 5  # Size of the validation set.
+SEED = 66478  # Set to None for random seed.
+BATCH_SIZE = 16  # 64
+NUM_EPOCHS = 100
+RESTORE_MODEL = False  # If True, restore existing model instead of training a new one
+RECORDING_STEP = 0
+PATCH_SIZE = 16
+#CRITERION = 
 
 # Loaded a set of images
 root_dir = "training/"
 
 image_dir = root_dir + "images/"
-files = os.listdir(image_dir)
-print("Loading images")
-imgs = [load_image(image_dir + files[i]) for i in range(len(files))]
-
 gt_dir = root_dir + "groundtruth/"
-print("Loading groundtruth images")
-gt_imgs = [load_image(gt_dir + files[i]) for i in range(len(files))]
+test_dir = "test_set_images/"
+
+train_set = ImgDataset(image_dir, gt_dir, split_ratio=0.8, mode="train")
+val_set = ImgDataset(image_dir, gt_dir, split_ratio=0.8, mode="val")
+test_set = TestDataset(test_dir)
+
+# Constructing the dataset from our map-style dataset
+train_loader = torch.utils.data.DataLoader(train_set, batch_size=BATCH_SIZE, shuffle=True)
+val_loader = torch.utils.data.DataLoader(val_set, batch_size=BATCH_SIZE, shuffle=False)
+test_loader = torch.utils.data.DataLoader(test_set, batch_size=BATCH_SIZE, shuffle=False)
+
+
 
