81b5d6de8acee12b81f0e5a6826a8450b3e10bfe
julia-walti <91602293+julia-walti@users.noreply.github.com>
2021-11-29 13:58:02 +0100

    Brouillon data augmentation

--- scripts/data_augmentation.py
+++ scripts/data_augmentation.py
@@ -1,2 +1,32 @@
-def DataAugm(image, gt):
+import numpy as np
+import os
+from PIL import Image, ImageOps
+from scipy import ndimage, misc
+
+class DataAugm():
     """ Get more data by rotating and flipping the images """
+    def __ini__(self, args, image, gt):
+
+    def rotation(degree):
+        # rotation
+        # import the Python Image processing Library
+        # Create an Image object from an Image
+        # Find a solution to open images here or pass adress to functions
+        colorImage  = Image.open("training/images/satImage_015.png")
+        # Rotate it by 45 degrees
+        img_45 = ndimage.rotate(colorImage, degree, reshape=False, mode='reflect')
+        gt_45 = ndimage.rotate(gt, degree, reshape=False, mode='reflect')
+
+    # flipping
+    def flip():
+        im_flip = ImageOps.flip(image)
+        gt_flip = ImageOps.flip(image)
+
+    # change in colors
+
+    # noise injection?
+
+
+
+for i in range()
+    im1 = im1.save("satImage_i_rotated_deg")
