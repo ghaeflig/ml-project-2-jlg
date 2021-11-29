7580e56141d9f17ec849e5ad69badacaa93deacc
ghaeflig <56826275+ghaeflig@users.noreply.github.com>
2021-11-29 11:39:00 +0100

    essai run

--- scripts/train.py
+++ scripts/train.py
@@ -6,7 +6,7 @@ import numpy as np
 #from tqdm import tqdm
 from torch.utils.tensorboard import SummaryWriter
 import torch.optim as optim
-#rom model import UNET
+from model import UNET
 from helpers import *
 
 # Hyperparameters etc.
@@ -26,8 +26,8 @@ PIN_MEMORY = True
 
 SEED = 66478
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
-NUM_EPOCHS = 100
-BATCH_SIZE = 16 
+NUM_EPOCHS = 3 #100
+BATCH_SIZE = 16 #64 
 LEARNING_RATE = 1e-4
 RESTORE_MODEL = False  # If True, restore existing model instead of training a new one
 OUTPUT_DIR = '../outputs/output_NE{}_BS{}_LR{}'.format(NUM_EPOCHS, BATCH_SIZE, LEARNING_RATE) # change in function of the paramters that are used to run the training 
@@ -57,11 +57,10 @@ def accuracy(pred, y) :
     return num_correct/num_pixels*100
 
 
-def train_func(train_loader, model, epoch, criterion, optimizer, writer=None, scaler=None, device=DEVICE, print_err=TRUE) : #rajouter scaler if cuda
+def train_func(train_loader, model, epoch, criterion, optimizer, scaler=None, writer=None, device=DEVICE) : #rajouter scaler if cuda
     model.train()
     train_loss = 0
     accuracies = 0
-    writer = SummaryWriter()
     # batch_x = train images and batch_y = train masks
     for it, (batch_x, batch_y) in ennumrate(train_loader) :  #for batch_x, batch_y in train_loader :
         batch_x, batch_y = batch_x.to(device), batch_y.to(device)
@@ -93,86 +92,101 @@ def train_func(train_loader, model, epoch, criterion, optimizer, writer=None, sc
     if writer is not None :
         writer.add_scalar('Train loss / epoch', train_loss, epoch)
         writer.add_scalar('Train accuracy / epoch', accuracy, epoch)
-    if print_err :
-         print("\nEpoch {} | Train loss: {:.5f} and train accuracy: {:.5f}".format(epoch, train_loss, accuracy))
+    return train_loss, accuracy
+
         
 
 
-def train_val(val_loader, epoch, device = DEVICE, print_err=TRUE) :
+def train_val(val_loader, model, epoch, writer=None, device = DEVICE) :
     model.eval()
     accuracies = 0
     with torch.no_grad() :
-    for batch_x, batch_y in val_loader :
-        batch_x, batch_y = batch_x.to(device), batch_y.to(device)
-
-        pred = torch.sigmoid(model(batch_x))
-        accuracies += accuracy(pred, batch_y)
-
-    accuracy = accuracies/len(val_loader)      
+        for batch_x, batch_y in val_loader :
+            batch_x, batch_y = batch_x.to(device), batch_y.to(device)
+            pred = torch.sigmoid(model(batch_x))
+            accuracies += accuracy(pred, batch_y)
+            
+    accuracy_avg = accuracies/len(val_loader)      
     if writer is not None :
-        writer.add_scalar("Validation accuracy / epoch", accuracy, epoch)
-    if print_err :
-        print("\nEpoch {} | Validation accuracy: {:.5f}".format(epoch, accuracy)
-              
+        writer.add_scalar("Validation accuracy / epoch", accuracy_avg, epoch)
+    return accuracy_avg
               
     # save a print overlay or not ??
-    """"img = img[0].permute(1,2,0).cpu().detach().numpy()
-    pred = pred[0].cpu().detach().numpy()
-    bin_mask, label_mask = gen_mask_label(pred, args)
-    merged_img, overlay = make_img_overlay(img, pred)
-    patched_img, overlay_patch = make_img_overlay(img, bin_mask)
-    merged_img.save(args.output_path + 'valid_merged_img.png')
-    patched_img.save(args.output_path + 'valid_patched_img.png')
-    overlay.save(args.output_path + 'valid_overlay.png')"""
-              
-    return accuracy
+    #img = img[0].permute(1,2,0).cpu().detach().numpy()
+    #pred = pred[0].cpu().detach().numpy()
+    #bin_mask, label_mask = gen_mask_label(pred, args)
+    #merged_img, overlay = make_img_overlay(img, pred)
+    #patched_img, overlay_patch = make_img_overlay(img, bin_mask)
+    #merged_img.save(args.output_path + 'valid_merged_img.png')
+    #patched_img.save(args.output_path + 'valid_patched_img.png')
+    #overlay.save(args.output_path + 'valid_overlay.png')
         
         
-def training() :
+def training(train_loader, val_loader, print_err=True) :
     print("\nStart training...")
     if not torch.cuda.is_available() :
         print("\nThings will go much quicker if you enable a GPU in Colab under 'Runtime / Change Runtime Type'")
     else : 
         print("\nYou are running the training of the data on a GPU")
     
-    train_loader, val_loader = get_loader(...)
     model = UNET(in_channels=3, out_channels=1).to(DEVICE)
     criterion = torch.nn.BCEWithLogitsLoss()
     optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE)
     #scheduler reduces learning rate when a metric has stopped improving
     #scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='max', factor=0.5, patience=7, verbose=True)
-    writer = SummaryWriter(comment="") # folder location: runs/May04_22-14-54_s-MacBook-Pro.comment/
+    writer = SummaryWriter() # folder location: runs/May04_22-14-54_s-MacBook-Pro.comment/ comment=''
+    scaler = None
     if torch.cuda.is_available(): 
-        scaler = torch.cuda.amp.GradScaler()
+        scaler_ = torch.cuda.amp.GradScaler()
         print("\nAs you are on a GPU, a scaler is added to the training in order to prevent underflow")
         
     for epoch in range(NUM_EPOCHS):
         max_accuracy = 0
-        train_func(train_loader, model, epoch, criterion, optimizer, witer, scaler)
-        accuracy = train_val(val_loader, epoch)
+        train_loss, accuracy_train = train_func(train_loader, model, epoch, criterion, optimizer, scaler, writer)
+        accuracy = train_val(val_loader, model, epoch, writer)
+        
+        if (print_err == True) :
+            print("\nEpoch {} | Train loss: {:.5f} and train accuracy: {:.5f}".format(epoch, train_loss, accuracy_train))
+            print("\nEpoch {} | Validation accuracy: {:.5f}".format(epoch, accuracy))    
         #scheduler.step()
         if accuracy > max_accuracy : 
-              max_accuracy = accuracy
-              max_accuracy_epoch = epoch
-              torch.save(model.state_dict(),  OUTPUT_DIR + '/parameters.pt') #.pt or .plk
-    
-    print("\nThe maximum accuracy over all epochs is {} at epoch {}".format(max_accuracy, epoch))
+            max_accuracy = accuracy.copy()
+            max_accuracy_epoch = epoch.copy()
+            torch.save(model.state_dict(),  OUTPUT_DIR + '/parameters.pt') #.pt or .plk
+        
+    print("\nThe maximum accuracy over all epochs is {} at epoch {}.".format(max_accuracy, epoch))
     print("\nThe best model over all epochs is saved into folder name {} with name parameters.pt".format(OUTPUT_DIR))
-          
-              
-""""def trained_model(output_dir=OUTPUT_DIR) :
-    path = "../outputs"
-    dirs = os.listdir(path)
-    where_equal = np.where(dirs == output_dir)
-    if len(where_equal) == 0 :
-        return True
-    else :
-        return False """"
+ 
+
+
+# def trained_model(output_dir=OUTPUT_DIR) :
+#    path = "../outputs"
+#    dirs = os.listdir(path)
+#    where_equal = np.where(dirs == output_dir)
+#    if len(where_equal) == 0 :
+#        return True
+#    else :
+#        return False 
+
+
 def trained_model(output_dir=OUTPUT_DIR) :
     return os.path.isdir(output_dir)
-              
-            
+
+                  
+"""
 def main() :
+    root_dir = "training/"
+
+    image_dir = root_dir + "images/"
+    gt_dir = root_dir + "groundtruth/"
+
+    train_set = ImgDataset(image_dir, gt_dir, split_ratio=0.8, mode="train")
+    val_set = ImgDataset(image_dir, gt_dir, split_ratio=0.8, mode="val")
+
+
+    train_loader = torch.utils.data.DataLoader(train_set, batch_size=BATCH_SIZE, shuffle=True)
+    val_loader = torch.utils.data.DataLoader(val_set, batch_size=BATCH_SIZE, shuffle=False)
+    
     if (trained_model() and not(RESTORE_MODEL)) : 
         print("\nThis model has already been trained and it will be loaded from the corresponding output folder...")
         model.load_state_dict(torch.load(OUTPUT_DIR + 'parameters.pt'))
@@ -181,8 +195,10 @@ def main() :
               print("\nYou choose to restore a model that has been already saved...")
         if (not(RESTORE_MODEL)) : 
               os.mkdir(OUTPUT_DIR)
-        training()
+        training(train_loader, val_loader)
 
               
 if __name__ == "__main__":
     main()
+"""
+
