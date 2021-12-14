80abf27aa562ae80bc886ae225267412fa9ea9d0
julia-walti <91602293+julia-walti@users.noreply.github.com>
2021-12-14 11:42:23 +0100

    concatenated overlay+gt ou pred

--- scripts/helpers.py
+++ scripts/helpers.py
@@ -14,57 +14,6 @@ def value_to_class(v, foreground_threshold):
     else:
         return 0
 
-def img_crop(im, w, h):
-    list_patches = []
-    imgwidth = im.shape[0]
-    imgheight = im.shape[1]
-    is_2d = len(im.shape) < 3
-    for i in range(0,imgheight,h):
-        for j in range(0,imgwidth,w):
-            if is_2d:
-                im_patch = im[j:j+w, i:i+h]
-            else:
-                im_patch = im[j:j+w, i:i+h, :]
-            list_patches.append(im_patch)
-    return list_patches
-
-def transform(imgs, gt_imgs):
-    # Compute features for each image patch
-    # Extract patches from input images
-    patch_size = 16 # each patch is 16*16 pixels
-
-    img_patches = [img_crop(imgs[i], patch_size, patch_size) for i in range(n)]
-    gt_patches = [img_crop(gt_imgs[i], patch_size, patch_size) for i in range(n)]
-
-    # Linearize list of patches
-    img_patches = np.asarray([img_patches[i][j] for i in range(len(img_patches)) for j in range(len(img_patches[i]))])
-    gt_patches =  np.asarray([gt_patches[i][j] for i in range(len(gt_patches)) for j in range(len(gt_patches[i]))])
-
-    foreground_threshold = 0.25 # percentage of pixels > 1 required to assign a foreground label to a patch
-
-    X = np.asarray([extract_features_2d(img_patches[i]) for i in range(len(img_patches))])
-    Y = np.asarray([value_to_class(np.mean(gt_patches[i]), foreground_threshold) for i in range(len(gt_patches))])
-
-    # Print feature statistics
-    print('Computed ' + str(X.shape[0]) + ' features')
-    print('Feature dimension = ' + str(X.shape[1]))
-    print('Number of classes = ' + str(np.max(Y)))  #TODO: fix, length(unique(Y))
-
-    Y0 = [i for i, j in enumerate(Y) if j == 0]
-    Y1 = [i for i, j in enumerate(Y) if j == 1]
-    print('Class 0: ' + str(len(Y0)) + ' samples')
-    print('Class 1: ' + str(len(Y1)) + ' samples')
-
-    # Display a patch that belongs to the foreground class i.e with ones
-    plt.imshow(gt_patches[Y1[2]], cmap='Greys_r')
-
-    # Display a patch that belongs to the background class i.e mostly zeros
-    #plt.imshow(gt_patches[Y0[2]], cmap='Greys_r')
-
-    # Balancing and extract features?
-    return img_patches, gt_patches
-
-
 ############################ MODEL EVALUATION PERFORMANCE ############################
 class IoULoss(torch.nn.Module):
     """Jaccard loss based on https://www.kaggle.com/bigironsphere/loss-function-library-keras-pytorch"""
@@ -72,22 +21,22 @@ class IoULoss(torch.nn.Module):
         super(IoULoss, self).__init__()
 
     def forward(self, inputs, targets, smooth=1):
-        
+
         #comment out if your model contains a sigmoid or equivalent activation layer
-        #inputs = torch.sigmoid(inputs)      
-        
+        #inputs = torch.sigmoid(inputs)
+
         #flatten label and prediction tensors
         inputs = inputs.view(-1)
         targets = targets.view(-1)
-        
+
         #intersection is equivalent to True Positive count
-        #union is the mutually inclusive area of all labels & predictions 
+        #union is the mutually inclusive area of all labels & predictions
         intersection = (inputs * targets).sum()
         total = (inputs + targets).sum()
-        union = total - intersection 
-        
+        union = total - intersection
+
         IoU = (intersection + smooth)/(union + smooth)
-                
+
         return 1 - IoU
 
 def check_accuracy(pred, y, batch_size=BATCH_SIZE):
@@ -111,8 +60,8 @@ def save_checkpoint(save_path, epoch, model, optimizer, scheduler) :
         saved_scheduler = scheduler.state_dict()
     checkpoint = {'epoch': epoch, 'model': model.state_dict(), 'optimizer': optimizer.state_dict(), 'scheduler': saved_scheduler}
     torch.save(checkpoint, save_path + '/parameters.pt')
-    
-    
+
+
 def load_checkpoint(checkpoint_path, model, optimizer = None, scheduler = None):
     if not os.path.exists(checkpoint_path):
         print("The requested file ({}) does not exist ; the checkpoint can not be loaded".format(checkpoint_path))
@@ -120,7 +69,7 @@ def load_checkpoint(checkpoint_path, model, optimizer = None, scheduler = None):
         checkpoint = torch.load(checkpoint_path, map_location=DEVICE)
         epoch = checkpoint['epoch']
         print("=> Loading checkpoint from a trained model at the best epoch {}".format(epoch))
-        model.load_state_dict(checkpoint['model']) 
+        model.load_state_dict(checkpoint['model'])
         if optimizer is not None :
             optimizer.load_state_dict(checkpoint['optimizer'])
         if scheduler is not None :
@@ -128,8 +77,7 @@ def load_checkpoint(checkpoint_path, model, optimizer = None, scheduler = None):
 
 
 ############################ PLOTS ###################################
-
-def make_img_overlay(img, predicted_img): 
+def make_img_overlay(img, predicted_img):
     w = img.shape[0]
     h = img.shape[1]
     color_mask = numpy.zeros((w, h, 3), dtype=numpy.uint8)
@@ -141,7 +89,24 @@ def make_img_overlay(img, predicted_img):
     new_img = Image.blend(background, overlay, 0.2)
     return new_img
 
-def img_float_to_uint8(img) : 
+def concatenate_images(img, gt_img):
+    """ Concatenate an image and its groundtruth """
+    nChannels = len(gt_img.shape)
+    w = gt_img.shape[0]
+    h = gt_img.shape[1]
+    if nChannels == 3:
+        cimg = np.concatenate((img, gt_img), axis=1)
+    else:
+        gt_img_3c = np.zeros((w, h, 3), dtype=np.uint8)
+        gt_img8 = img_float_to_uint8(gt_img)
+        gt_img_3c[:,:,0] = gt_img8
+        gt_img_3c[:,:,1] = gt_img8
+        gt_img_3c[:,:,2] = gt_img8
+        img8 = img_float_to_uint8(img)
+        cimg = np.concatenate((img8, gt_img_3c), axis=1)
+    return cimg
+
+def img_float_to_uint8(img) :
     """transform img from float to uint8"""
     rimg = img - np.min(img)
     rimg = (rimg / np.max(rimg) * 255).round().astype(np.uint8)
--- scripts/test.py
+++ scripts/test.py
@@ -1,277 +1,73 @@
-import matplotlib.image as mpimg
 import numpy as np
-import matplotlib.pyplot as plt
 import os, sys
-import torch.optim as optim
-import copy
-from torch.utils import data
-from dataset import ImgDataset
+import torch
+import skimage.io as io
 from dataset import TestDataset
-from helpers import *
+from helpers import load_checkpoint
 from model import UNET
-from torch.utils.tensorboard import SummaryWriter
-from sklearn.metrics import f1_score, accuracy_score
+from mask_to_submission import *
+from train import DEVICE, BATCH_SIZE, OUTPUT_DIR
+#from submission_to_mask import *
+
+IMG_PLOTS = False #save one test image with overlay and genrated mask
+IMG = 6 #index of the image saved, can be between 1 and 50
+
+print('\nStart running...')
+if not torch.cuda.is_available() :
+	print("\nThings will go much quicker if you enable a GPU in Colab under 'Runtime / Change Runtime Type'")
+else :
+	print("\nYou are running the prediction of the test data on a GPU")
+
+# Load the test set
+print('\nTest data loading...')
+test_dir = "../data/test_set_images/"
+test_set = TestDataset(test_dir)
+test_loader = torch.utils.data.DataLoader(test_set, batch_size=BATCH_SIZE, shuffle=False)
 
 
+# Load checkpoint
+root_dir = "../outputs"
+checkpoint_path = os.path.join(root_dir, 'parameters.pt')
+model = UNET().to(DEVICE)
+load_checkpoint(checkpoint_path, model)
 
-# Hyperparameters etc.
-""""
-NUM_CHANNELS = 3  # RGB images
-PIXEL_DEPTH = 255
-NUM_LABELS = 2
-TRAINING_SIZE = 20
-VALIDATION_SIZE = 5  # Size of the validation set.
-RECORDING_STEP = 0
-PATCH_SIZE = ?
-NUM_WORKERS = 2
-IMAGE_HEIGHT = 160  # 1280 originally
-IMAGE_WIDTH = 240  # 1918 originally
-PIN_MEMORY = True
-"""
+# Model evaluation
+model.eval()
 
-
-SEED = 66478
-DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
-NUM_EPOCHS = 75 #100 #75
-BATCH_SIZE = 2 #64
-LEARNING_RATE = 1e-4
-WEIGHT_DECAY = 0
-#RESTORE_MODEL = False  # If True, restore existing model instead of training a new one
-#OUTPUT_DIR = '../outputs/output_NE{}_BS{}_LR{}_WD{}'.format(NUM_EPOCHS, BATCH_SIZE, LEARNING_RATE, WEIGHT_DECAY) # change in function of the paramters that are used to run the training
-# a trouver un bon moyen de nommer nos outputs folders
-OUTPUT_DIR = '../outputs'
-
-torch.manual_seed(SEED) # a voir si c'est deja fait a quelque part --> eviter les problème de dépendance
-
-"""def check_accuracy(pred, y):
-    pred = (pred > 0.5).float()
-    num_correct = 0
-    num_pixels = 0
-    num_correct += (pred == y).sum()
-    num_pixels += torch.numel(pred)
-    return num_correct/num_pixels"""
-
-def img_float_to_uint8(img):
-    rimg = img - np.min(img)
-    rimg = (rimg / np.max(rimg) * 255).round().astype(np.uint8)
-    return rimg
+# Make and save test binary prediction images
+save_path = os.path.join(root_dir, 'binary_masks')
+if not os.path.isdir(save_path) :
+	os.mkdir(save_path)
+it = 1
+print('\nDo not care about the warnings...')
+for batch_x in test_loader :
+	batch_x = batch_x.permute(0, 3, 1, 2).float()
+	batch_x = batch_x.to(DEVICE)
+	pred = model(batch_x)
+	pred = pred.squeeze(1)
+	pred = torch.sigmoid(pred)
+	for i in range(BATCH_SIZE) :
+		mask = pred[i].cpu().detach().numpy() #we can not convert cuda tensor into numpy
+		mask[mask>0.5] = 1
+		mask[mask<=0.5] = 0
+		mask_path = os.path.join(save_path, '%.3d' % it + '.png')
+		io.imsave(mask_path, mask, check_contrast=False)
+		print('Prediction binary mask for test image {} is saved'.format(it))
+        # Make some plot if IMG_PLOTS = true
+        if (IMG_PLOTS and it==IMG):
+            mask_path = os.path.join(root_dir, 'result_ex%.3d' % IMG + '.png')
+            img_overlay =  make_img_overlay(batch_x[i], pred)
+            io.imsave(mask_path, img_overlay)
+            cimg = concatenate_images(img_overlay, pred)
+            fig1 = plt.figure(figsize=(10, 10))
+            plt.imshow(cimg, cmap='Greys_r')
+            print('Concatenated image with overlay and mask saved in {}'.format(mask_path))
+		it = it + 1
 
 
-def concatenate_images(img, gt_img):
-    nChannels = len(gt_img.shape)
-    img = img.permute(2, 1, 0).float()
-    w = gt_img.shape[0]
-    h = gt_img.shape[1]
-    if nChannels == 3:
-        cimg = np.concatenate((img, gt_img), axis=1)
-    else:
-        #gt_img_3c = np.zeros((w, h, 3), dtype=np.uint8)
-        gt_img_3c = np.zeros((w, h, 3))
-        #gt_img8 = img_float_to_uint8(gt_img)
-        gt_img_3c[:,:,0] = gt_img.detach().cpu().numpy() #PETITE CORRECTION .cpu()
-        gt_img_3c[:,:,1] = gt_img.detach().cpu().numpy()
-        gt_img_3c[:,:,2] = gt_img.detach().cpu().numpy()
-        #img8 = img_float_to_uint8(img)
-        #print(type(img))
-        #print(type(gt_img_3c))
-        cimg = np.concatenate((img.cpu(), gt_img_3c), axis=1)
-    return cimg
+print('\nYou can find all the test binary masks by following the path : {}'.format(save_path))
 
-
-
-
-
-def check_accuracy(pred, y, batch_size=BATCH_SIZE):
-    pred = (pred > 0.5).float()
-    patch_pred = [img_crop(pred[i].cpu().detach().numpy(), 16, 16) for i in range(batch_size)]
-    patch_y = [img_crop(y[i].cpu().detach().numpy(), 16, 16) for i in range(batch_size)]
-    return accuracy_score(np.array(patch_y).ravel(), np.array(patch_pred).ravel())
-
-
-
-def check_f1(pred, y, batch_size=BATCH_SIZE):
-    pred = (pred > 0.5).float()
-    patch_pred = [img_crop(pred[i].cpu().detach().numpy(), 16, 16) for i in range(batch_size)]
-    patch_y = [img_crop(y[i].cpu().detach().numpy(), 16, 16) for i in range(batch_size)]
-    return f1_score(np.array(patch_y).ravel(), np.array(patch_pred).ravel())
-
-
-
-def train_func(train_loader, model, epoch, criterion, optimizer, scaler=None, writer=None, device=DEVICE) :
-    model.train()
-    train_loss = 0
-    accuracies = 0
-    f1_sum = 0
-    it = 0
-    #batch_x = train images and batch_y = train masks
-    for batch_x, batch_y in train_loader:
-        print("it num for train func : {} / {}.".format(it, len(train_loader)-1))
-        batch_x = batch_x.permute(0, 3, 2, 1).float()
-        batch_x, batch_y = batch_x.to(device), batch_y.to(device)
-
-        # Evaluate the network (forward pass)
-        pred = model(batch_x)
-        pred = pred.squeeze(1) #[8,1,400,400] to [8,400,400] to have the same size as batch_y
-
-        if (epoch==18):
-            mask = pred[0].cpu().detach().numpy() #we can not convert cuda tensor into numpy
-            #print(mask.type())
-            mask[mask>0.5] = 1
-            mask[mask<=0.5] = 0
-            #mask = img_float_to_uint8(mask)
-            mask_path = os.path.join(save_path, '%.3d' % it + '.png')
-            print(mask_path)
-            io.imsave(mask_path, mask)
-            print('Prediction binary mask for test image {} is saved'.format(it))
-
-         # Compute the loss and the gradient
-        loss = criterion(pred, batch_y) #pas sigmoid dans la binary cross entropy donc sigmoid apres
-        train_loss += float(loss.item())
-        #pred = torch.sigmoid(pred)
-        #print("pred after sigmoid :", pred)
-        accuracies += check_accuracy(pred, batch_y)
-        #print("f1_sum before check_f1 :", f1_sum)
-        f1_sum += check_f1(pred, batch_y)
-        #print("f1_sum after check_f1 :", f1_sum)
-
-        # print to check results
-        cimg = concatenate_images(batch_x[0], pred[0])
-        fig1 = plt.figure(figsize=(10, 10))
-        plt.imshow(cimg, cmap='Greys_r')
-
-        optimizer.zero_grad()
-        loss.backward()
-
-        # Scaler prevents underflow ; gradient values have a larger magnitude, so they don’t flush to zero
-        if scaler is not None :
-            scaler.scale(loss).backward()
-            scaler.step(optimizer)
-            scaler.update()
-
-        # Update the parameters of the model with a gradient step
-        optimizer.step()
-
-        #if writer is not None:
-            #writer.add_scalar("Train loss / batch for epoch {}".format(epoch), loss.item(), it)
-        it = it + 1
-
-    #print("f1_sum after running on batch :", f1_sum)
-    train_loss = train_loss / len(train_loader)
-    accuracy_mean = accuracies / len(train_loader)
-    f1_mean = f1_sum / len(train_loader) #len = 40
-    #if writer is not None :
-    #    writer.add_scalar('Train loss / epoch', train_loss, epoch)
-    #    writer.add_scalar('Train accuracy / epoch', accuracy_mean, epoch)
-
-    return train_loss, accuracy_mean, f1_mean
-
-
-def train_val(val_loader, model, epoch, writer=None, device = DEVICE) :
-    model.eval()
-    accuracies = 0
-    f1_sum = 0
-    it = 0
-    with torch.no_grad() :
-        for batch_x, batch_y in val_loader :
-            print("it num for train val func : {} / {}.".format(it, len(val_loader)-1))
-            batch_x = batch_x.permute(0, 3, 2, 1).float()
-            batch_x, batch_y = batch_x.to(device), batch_y.to(device)
-            pred = model(batch_x)
-            pred = pred.squeeze(1)
-            pred = torch.sigmoid(pred)
-            accuracies += check_accuracy(pred, batch_y)
-            f1_sum += check_f1(pred, batch_y)
-            it = it + 1
-
-    accuracy_mean = accuracies/len(val_loader)
-    f1_mean = f1_sum / len(val_loader)
-    #if writer is not None :
-    #    writer.add_scalar("Validation accuracy / epoch", accuracy_avg, epoch)
-    return accuracy_mean, f1_mean
-
-
-def training(train_loader, val_loader, print_err=True) :
-    print("\nStart training...")
-    if not torch.cuda.is_available() :
-        print("\nThings will go much quicker if you enable a GPU in Colab under 'Runtime / Change Runtime Type'")
-    else :
-        print("\nYou are running the training of the data on a GPU")
-
-    max_f1 = 0
-    model = UNET().to(DEVICE) # peut etre a mettre model en parametres de la fonction
-    criterion = torch.nn.BCEWithLogitsLoss().to(DEVICE)
-    optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE, weight_decay=WEIGHT_DECAY)
-    # scheduler reduces learning rate when a metric has stopped improving
-    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='max', factor=0.5, patience=7, verbose=True) #rajouter variable globale factor ect..
-    writer = None #SummaryWriter() # folder location: runs/May04_22-14-54_s-MacBook-Pro.comment/ comment=''
-    scaler = None
-    #if torch.cuda.is_available():
-    #    scaler_ = torch.cuda.amp.GradScaler()
-    #    print("\nAs you are on a GPU, a scaler is added to the training in order to prevent underflow")
-
-    for epoch in range(NUM_EPOCHS):
-        print("\nEpoch : {} / {}".format(epoch, NUM_EPOCHS-1))
-        train_loss, accuracy_train, f1_train = train_func(train_loader, model, epoch, criterion, optimizer, scaler, writer)
-        accuracy_val, f1_val = train_val(val_loader, model, epoch, writer)
-        scheduler.step(train_loss) # or eval score ?
-
-        if (print_err == True) :
-            print("\nEpoch {} | Train loss: {:.5f} train f1 score: {:.5f} and train accuracy: {:.5f}".format(epoch, train_loss, f1_train, accuracy_train))
-            print("Epoch {} | Validation f1 score : {:.5f} and validation accuracy: {:.5f}".format(epoch, f1_val, accuracy_val))
-
-        if f1_val > max_f1 :
-            max_f1 = copy.deepcopy(f1_val) #à verifier
-            max_f1_epoch = copy.deepcopy(epoch)
-            save_checkpoint(OUTPUT_DIR, epoch, model, optimizer, scheduler, scaler)
-            #torch.save(model.state_dict(),  OUTPUT_DIR + '/parameters.pt') #.pt or .plk
-
-    print("\nThe maximum f1 score over all epochs is {} at epoch {}.".format(max_f1, max_f1_epoch))
-    print("\nThe best model over all epochs is saved into folder name {} with name parameters.pt".format(OUTPUT_DIR))
-
-
-
-# def trained_model(output_dir=OUTPUT_DIR) :
-#    path = "../outputs"
-#    dirs = os.listdir(path)
-#    where_equal = np.where(dirs == output_dir)
-#    if len(where_equal) == 0 :
-#        return True
-#    else :
-#        return False
-
-
-def trained_model(output_dir=OUTPUT_DIR) :
-    return os.path.isdir(output_dir)
-
-def main() :
-    root_dir = "../data/training/"
-
-    image_dir = root_dir + "images/"
-    gt_dir = root_dir + "groundtruth/"
-
-    train_set = ImgDataset(image_dir, gt_dir, split_ratio=0.8, mode="train")
-    val_set = ImgDataset(image_dir, gt_dir, split_ratio=0.8, mode="val")
-
-
-    train_loader = torch.utils.data.DataLoader(train_set, batch_size=BATCH_SIZE, shuffle=True)
-    val_loader = torch.utils.data.DataLoader(val_set, batch_size=BATCH_SIZE, shuffle=False)
-
-    """if (trained_model() and not(RESTORE_MODEL)) :
-        print("\nThis model has already been trained and the parameters can be loaded from the corresponding output folder : {}".format(OUTPUT_DIR))
-        #model.load_state_dict(torch.load(OUTPUT_DIR + 'parameters.pt'))
-    else :
-        if (RESTORE_MODEL and trained_model()) :
-              print("\nYou choose to restore a model that has been already saved...")
-              print("\nThe results will be in the folder whose name is : {}".format(OUTPUT_DIR))
-              os.mkdir(OUTPUT_DIR + '_restored')
-        if (not(RESTORE_MODEL)) :
-              os.mkdir(OUTPUT_DIR)
-              print("\n Creating the output directory : {} in which you will find the parameters of the trained model (parameters.pt)".format(OUTPUT_DIR))
-        training(train_loader, val_loader)"""
-
-    training(train_loader, val_loader)
-
-
-
-if __name__ == "__main__":
-    main()
+# Make a submission csv file
+print('\nTransformation of the binary masks into a submission csv file...')
+submission(save_path, root_dir)
+print('\nYou can find the submission.csv file by following the path : {}'.format(root_dir))
