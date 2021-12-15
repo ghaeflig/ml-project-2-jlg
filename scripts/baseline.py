30a6e9b7ae7e81d2f5f29c81b16dfa886380d7f9
julia-walti <91602293+julia-walti@users.noreply.github.com>
2021-12-15 11:36:50 +0100

    baseline MLP

--- scripts/baseline.py
+++ scripts/baseline.py
@@ -3,88 +3,41 @@ import scipy
 import scipy.io
 import scipy.sparse as sp
 import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+from sklearn.metrics import f1_score, accuracy_score
 from sklearn.neural_network import MLPClassifier
-from sklearn.metrics import accuracy_score
 
 import os, sys
 import torch.optim as optim
 import copy
-from dataset import ImgDataset 
-from helpers import *
+################################ HELPERS ####################################
+def value_to_class(v):
+    foreground_threshold = 0.25  # percentage of pixels > 1 required to assign a foreground label to a patch
+    df = np.sum(v)
+    if df > foreground_threshold:  # road
+        return [0, 1]
+    else:  # bgrd
+        return [1, 0]
 
+def extract_features_2d(img):
+    feat_m = np.mean(img)
+    feat_v = np.var(img)
+    feat = np.append(feat_m, feat_v)
+    return feat
 
-#Baseline : MLP model with feedforward and backpropagation
-
-def sigmoid(t):
-    """Apply sigmoid function on t.
-    Args:       t: scalar or numpy array
-    Returns:    scalar or numpy array"""
-
-    return 1.0 / (1 + np.exp(-t))
-
-
-def grad_sigmoid(t):
-    """Return the gradient of sigmoid on t.
-    Args:       t: scalar or numpy array
-    Returns:    scalar or numpy array"""
-        
-    return sigmoid(t) * (1 - sigmoid(t))
-
-#Model :  
-#Input layer : number of neurons comprising that layer is equal to the number of features (columns) in your data --> 3
-#Output layer : since NN is a classifier, then it has a single node --> 1
-#Hidden layers : 2/3 the size of the input layer + the size of the output layer --> 3
-""""
-x = np.array of shape (D, ), with D=3
-W = {
-    "w_1": np.ones((3, 3)),
-    "w_2": np.ones(3)
-}
-y = 1
-"""
-
-def simple_feed_forward(x, W):
-    """Do feed-forward propagation.
-    Args:
-        x: numpy array of shape (D, )
-        W: a dictionary of numpy array, with two elements, w_1 and w_2.
-            w_1: shape=(D, K)
-            w_2: shape=(K, )
-    Returns:
-        z1: a numpy array, generated from the hidden layer (before the sigmoid function) 
-        z2: scalar number, generated from the output layer (before the sigmoid function)
-        y_hat: a scalar (after the sigmoid function)"""
-    
-    x_0 = x
-    z_1 = W["w_1"].T @ x_0
-    x_1 = sigmoid(z_1)
-    z_2 = W["w_2"].T @ x_1
-    y_hat = sigmoid(z_2)
-    
-    return z_1, z_2, y_hat
-
-
-def simple_backpropagation(y, x, W):
-    """Do backpropagation and get delta_W.
-    Args:
-        y: scalar number
-        x: numpy array of shape (D, )
-        W: a dictionary of numpy array, with two elements, w_1 and w_2.
-            w_1: shape=(D, K)
-            w_2: shape=(K, )
-    Returns:
-        grad_W: a dictionary of numpy array. It corresponds to the gradient of weights in W."""
-    
-    # Feed forward
-    z_1, z_2, y_hat = simple_feed_forward(x, W)
-    x_1 = sigmoid(z_1)
-    # Backpropogation
-    delta_2 = (y_hat - y) * grad_sigmoid(z_2)
-    delta_w_2 = delta_2 * x_1
-    delta_1 = delta_2 * W["w_2"] * grad_sigmoid(z_1)
-    delta_w_1 = np.outer(x, delta_1)
-    
-    return {"w_2": delta_w_2, "w_1": delta_w_1}
+def img_crop(im, w, h):
+    list_patches = []
+    imgwidth = im.shape[0]
+    imgheight = im.shape[1]
+    is_2d = len(im.shape) < 3
+    for i in range(0,imgheight,h):
+        for j in range(0,imgwidth,w):
+            if is_2d:
+                im_patch = im[j:j+w, i:i+h]
+            else:
+                im_patch = im[j:j+w, i:i+h, :]
+            list_patches.append(im_patch)
+    return list_patches
 
 
 
@@ -93,95 +46,87 @@ def main() :
     root_dir = "../data/training/"
     image_dir = root_dir + "images/"
     gt_dir = root_dir + "groundtruth/"
-    train_set = ImgDataset(image_dir, gt_dir, split_ratio=0.8, mode="train")
-    val_set = ImgDataset(image_dir, gt_dir, split_ratio=0.8, mode="val")
-    train_loader = torch.utils.data.DataLoader(train_set, batch_size=BATCH_SIZE, shuffle=True)
-    val_loader = torch.utils.data.DataLoader(val_set, batch_size=BATCH_SIZE, shuffle=False)
+    test_dir = "../data/test/"
 
-    clf = MLPClassifier(max_iter=1000,
-                    hidden_layer_sizes=(3,),
+    files = os.listdir(image_dir)
+    print("Loading images")
+    images = np.asarray([mpimg.imread(image_dir + files[i]) for i in range(len(files))])
+    print("Loading groundtruth images")
+    gt = np.asarray([mpimg.imread(gt_dir + files[i]) for i in range(len(files))])
+
+    # Use 16x16 patches
+    img_patches = [img_crop(images[i], 16, 16) for i in range(len(files))]
+    gt_patches = [img_crop(gt[i], 16, 16) for i in range(len(files))]
+    # Linearize list of patches
+    img_patches = np.asarray([img_patches[i][j] for i in range(len(img_patches)) for j in range(len(img_patches[i]))])
+    gt_patches =  np.asarray([gt_patches[i][j] for i in range(len(gt_patches)) for j in range(len(gt_patches[i]))])
+    labels = np.asarray([value_to_class(np.mean(gt_patches[i])) for i in range(len(gt_patches))])
+    # Convert to dense 1-hot representation.
+    labels.astype(np.float32)
+
+    # Separate into training and validation set
+    n = 0.8 # split ratio for train/val
+    N = len(labels)
+    idx = np.floor(n*N).astype('int32') # index to stop training set
+    X_train = np.asarray([extract_features_2d(img_patches[i]) for i in np.arange(idx)])
+    print(X_train.shape)
+    Y_train = np.asarray([labels[i] for i in np.arange(idx)])
+
+    X_val = np.asarray([extract_features_2d(img_patches[i]) for i in np.arange(idx+1, N)])
+    Y_val = np.asarray([value_to_class(np.mean(labels[i])) for i in np.arange(idx+1, N)])
+
+    # Balance the training data if needed
+    c0 = 0  # bgrd
+    c1 = 0  # road
+    for i in range(len(Y_train)):
+        if Y_train[i][0] == 1:
+            c0 = c0 + 1
+        else:
+            c1 = c1 + 1
+    print('Number of data points per class before balancing: c0 = ' + str(c0) + ' c1 = ' + str(c1))
+    print('Balancing training data...')
+    min_c = min(c0, c1)
+    idx0 = [i for i, j in enumerate(Y_train) if j[0] == 1]
+    idx1 = [i for i, j in enumerate(Y_train) if j[1] == 1]
+    new_indices = idx0[0:min_c] + idx1[0:min_c]
+    X_train = X_train[new_indices, :]
+    Y_train = Y_train[new_indices]
+    c0 = 0
+    c1 = 0
+    for i in range(len(Y_train)):
+        if Y_train[i][0] == 1:
+            c0 = c0 + 1
+        else:
+            c1 = c1 + 1
+    train_size = Y_train.shape[0]
+    print('Number of data points per class after balancing: c0 = ' + str(c0) + ' c1 = ' + str(c1))
+
+    # Fitting the model
+    clf = MLPClassifier(max_iter=200,
+                    hidden_layer_sizes=(10, 10),
                     random_state=5,
-                    verbose=True,
-                    learning_rate_init=0.01)
-    clf.fit(train_features, train_labels)
+                    verbose=False,
+                    learning_rate_init=1e-4,
+                    learning_rate = 'adaptive')
 
-    ypred=clf.predict(test_features)
-    
-    accuracy_score(test_labels, ypred)
+    clf.fit(X_train, Y_train)
+
+
+    # Testing the model with a validation set
+    Y_pred = clf.predict(X_val)
+    #Y_pred[Y_pred>0.5] = 1
+    #Y_pred[Y_pred<=0.5]= 0
+    #print('Y val: {} and Y_pred: {}'.format(Y_val, Y_pred))
+    c0 = 0
+    c1 = 0
+
+    acc0 = accuracy_score(Y_val[:,0], Y_pred[:,0])
+    acc1 = accuracy_score(Y_val[:,1], Y_pred[:,1])
+    f1 = f1_score(Y_val, Y_pred, average = 'micro')
+
+    print('F1 score {} and accuracy 0 {} and acuracy 1 {}'.format(f1, acc0, acc1))
+
+
 
 if __name__ == "__main__":
     main()
-
-
-
-
-""" ------------------------------ TEST MAIN 2 --------------------------------
-def main() :
-    # Loading the data
-    root_dir = "../data/training/"
-    image_dir = root_dir + "images/"
-    gt_dir = root_dir + "groundtruth/"
-    train_set = ImgDataset(image_dir, gt_dir, split_ratio=0.8, mode="train")
-    val_set = ImgDataset(image_dir, gt_dir, split_ratio=0.8, mode="val")
-    train_loader = torch.utils.data.DataLoader(train_set, batch_size=BATCH_SIZE, shuffle=True)
-    val_loader = torch.utils.data.DataLoader(val_set, batch_size=BATCH_SIZE, shuffle=False)
-
-    train_features, train_labels = next(iter(train_loader))
-    print(f"Feature batch shape: {train_features.size()}")
-    print(f"Labels batch shape: {train_labels.size()}")
-
-    clf = MLPClassifier(max_iter=1000,
-                    hidden_layer_sizes=(3,),
-                    random_state=5,
-                    verbose=True,
-                    learning_rate_init=0.01)
-    clf.fit(train_features, train_labels)
-
-    
-    test_features, test_labels = next(iter(val_loader))
-    ypred=clf.predict(test_features)
-    
-    accuracy_score(test_labels, ypred)
-"""
-
-
-
-
-""" ------------------------------ TEST MAIN 3 --------------------------------
-def main() :
-    # Loading the data
-    root_dir = "../data/training/"
-    image_dir = root_dir + "images/"
-    gt_dir = root_dir + "groundtruth/"
-    train_set = ImgDataset(image_dir, gt_dir, split_ratio=0.8, mode="train")
-    val_set = ImgDataset(image_dir, gt_dir, split_ratio=0.8, mode="val")
-    train_loader = torch.utils.data.DataLoader(train_set, batch_size=BATCH_SIZE, shuffle=True)
-    val_loader = torch.utils.data.DataLoader(val_set, batch_size=BATCH_SIZE, shuffle=False)
-
-    train_features, train_labels = next(iter(train_loader))
-    #img = train_features[0].squeeze()
-    #label = train_labels[0]
-    print(f"Feature batch shape: {img.size()}")
-    print(f"Labels batch shape: {label.size()}")
-    
-    nsamples, nx, ny = img.shape
-    d2_img = img.reshape((nsamples,nx*ny))
-
-    clf = MLPClassifier(max_iter=1000,
-                    hidden_layer_sizes=(3,),
-                    random_state=5,
-                    verbose=True,
-                    learning_rate_init=0.01)
-    clf.fit(d2_img, label)
-
-    
-    test_features, test_labels = next(iter(val_loader))
-    img_test = test_features[0].squeeze()
-    label_test = test_labels[0]
-
-    nsamples, nx, ny = img_test.shape
-    d2_img_test = img_test.reshape((nsamples,nx*ny))
-    ypred=clf.predict(d2_img_test)
-    
-    accuracy_score(label_test, ypred)
-"""
