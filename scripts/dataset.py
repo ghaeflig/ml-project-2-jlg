29d6c9a8d97042bdc1b2a8a7208ffe4ce0f498da
julia-walti <91602293+julia-walti@users.noreply.github.com>
2021-12-22 18:56:20 +0100

    dataset relu

--- scripts/dataset.py
+++ scripts/dataset.py
@@ -3,32 +3,26 @@ import numpy as np
 import torch
 from PIL import Image
 from torch.utils.data import Dataset
-#from helpers import value_to_class
-#from data_augmentation import transform
 
 SEED = 66478
 torch.manual_seed(SEED)
 
 class ImgDataset(Dataset):
-    """ Dataset loader
-    train or validation mode
+    """ Dataset loader - Train or validation mode
     """
     def __init__(self, image_dir, gt_dir, mode="train", split_ratio=0.8):
         self.mode = mode
         self.image_dir = image_dir
         self.gt_dir = gt_dir
-        #self.transform = transform
 
-        #ids = [os.path.splitext(file)[0] for file in os.listdir(image_dir)]
         ids = [int(file[9:12]) for file in os.listdir(image_dir)]
         n = len(ids)
         if self.mode == "train" :
             self.ids = ids[0:int(n * split_ratio)]
         if self.mode == "val":
             self.ids = ids[int(n * split_ratio):n]
-            #print(f'ids: {self.ids}')
-            #print(f'TEST index 0 val: {self.ids[0]-1}')
         self.ids.sort()
+
         self.images = os.listdir(image_dir) #list all files in that folder
         self.gt = os.listdir(gt_dir)
         print(f'Creating {mode} dataset with {len(self.ids)} samples')
@@ -37,14 +31,11 @@ class ImgDataset(Dataset):
         return len(self.ids)
 
     def __getitem__(self, index):
-        #print(f'index: {index}')
         idx = self.ids[index]-1 # bc index [0,99] and ids [1,100] and images[idx] needs [0,99]
-        #print(f'idx: {idx}')
         image_path = os.path.join(self.image_dir, self.images[idx])
         gt_path = os.path.join(self.gt_dir, self.images[idx])
         image = np.array(Image.open(image_path).convert("RGB"))
         gt = np.array(Image.open(gt_path).convert("L"), dtype=np.float32)
-        #gt = np.expand_dims(gt, 0)
         gt[gt>0.5] = 1
         gt[gt<=0.5] = 0
         return image, gt
